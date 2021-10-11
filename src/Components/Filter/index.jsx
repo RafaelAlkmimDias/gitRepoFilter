f13bb52c5e0da4c0e491d7fc2fe2ba3dab7105ae
Rafael de Alkmim Dias <rafas07@hotmail.com>
2021-10-11 08:03:15 -0300

    filter style

--- src/Components/Filter/index.jsx
+++ src/Components/Filter/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Wrapper, Button, WrapperInput, InputLabel, Input, Select} from './style';
+import {Wrapper, Button, WrapperInput, InputLabel, Input, Select, WrapperData, WrapperSelect} from './style';
 
 import { connect } from 'react-redux';
 import { submitFilter, changeInput } from '../../Redux/Store/action'
@@ -19,28 +19,30 @@ const Filter = ({ filter, filterText, dispatch }) => {
 
     return(
         <Wrapper>
-            <WrapperInput>
-                <InputLabel htmlFor="filter">
-                    Pesquisar por:
-                </InputLabel>
-                <Select name="filter" id="filter" onChange={onSelectChange} value={filter}>
-                    <option value="orgs"> Organização </option>
-                    <option value="users"> Usuário </option>
-                </Select>
-            </WrapperInput>
-            <WrapperInput>
-                <InputLabel htmlFor="filterText">
-                    Pesquisar:
-                </InputLabel>
-                <Input 
-                    type="text" 
-                    name="filterText"
-                    id="filterText"
-                    value={filterText}
-                    onChange={onTextChange}
-                />
-            </WrapperInput>
+            <WrapperData>
+                <WrapperSelect>
+                    <InputLabel htmlFor="filter">
+                        Pesquisar por:
+                    </InputLabel>
+                    <Select name="filter" id="filter" onChange={onSelectChange} value={filter}>
+                        <option value="orgs"> Organização </option>
+                        <option value="users"> Usuário </option>
+                    </Select>
+                </WrapperSelect>
 
+                <WrapperInput>
+                    <InputLabel htmlFor="filterText">
+                        Pesquisar:
+                    </InputLabel>
+                    <Input 
+                        type="text" 
+                        name="filterText"
+                        id="filterText"
+                        value={filterText}
+                        onChange={onTextChange}
+                    />
+                </WrapperInput>
+            </WrapperData>
             <Button onClick={() => submit() } >
                 Pesquisar    
             </Button>
--- src/Components/Filter/style.jsx
+++ src/Components/Filter/style.jsx
@@ -2,26 +2,56 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     padding: 20px;
-    border: 1px solid black;
+    border: 1px solid #d0d7de;
+    border-radius: 7px;
+    background-color: #fff;
+`;
+
+export const WrapperData = styled.div`
+    display: flex;
+    flex-direction: row;
+    @media(max-width: 640px) {
+        flex-direction: column;
+    }
 `;
 
 export const Button = styled.button`
-
+    width: 100%;
+    padding: 10px;
+    background-color: #000;
+    color: #fff;
+    border: none;
+    border-radius: 8px;
 `;
 
 
 export const WrapperInput = styled.div`
+    padding: 0px 0px 8px 8px;
+    width: 100%;
+    @media(max-width: 640px) {
+        padding: 0px 0px 8px 0px;
+    }
+`;
+export const WrapperSelect = styled.div`
     padding: 0px 0px 8px 0px;
+    @media(max-width: 640px) {
+        width: 100%;
+    }
 `;
 
 export const InputLabel = styled.label`
     display: block;
+    line-height: 28px;
 `;
 
 export const Input = styled.input`
     width: 100%;
+    padding: 6px;
 `;
 
 export const Select = styled.select`
-
+    padding: 6px;
+    @media(max-width: 640px) {
+        width: 100%;
+    }
 `;
